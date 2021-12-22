d3f388fd64bec00ba25e3d66d72690cc2bc0fa6e
Chlamydomonos <2457242458@qq.com>
2021-12-22 12:25:20 +0800

    Initial commit

---
+++ src/main/java/xyz/chlamydomonos/chlamydomonosia/chlash/Chlash.java
@@ -0,0 +1,9 @@
+package xyz.chlamydomonos.chlamydomonosia.chlash;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Chlash.MODID)
+public class Chlash
+{
+    public static final String MODID = "chlash";
+};
---
+++ src/main/java/xyz/chlamydomonos/chlamydomonosia/chlash/HashTable.java
@@ -0,0 +1,13 @@
+package xyz.chlamydomonos.chlamydomonosia.chlash;
+
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.HashMap;
+
+public class HashTable extends HashMap<ResourceLocation, Integer>
+{
+    public HashTable()
+    {
+        super();
+    }
+}
